f6c10005864036ca737d22e0004f88d1b476bf35
ericjonesdev <ericjones2004@yahoo.com>
2022-12-28 13:24:40 +0000

    Collect sales data for last 5 markets in lists of columns correction

--- run.py
+++ run.py
@@ -88,13 +88,13 @@ def get_last_5_entries_sales():
     as a list of lists.
     """
     sales = SHEET.worksheet("sales")
-    # column = sales.col_values(3)
-    # print(column)
+    
     columns = []
     for ind in range(1, 7):
         column = sales.col_values(ind)
         columns.append(column[-5:])
-        pprint(columns)
+        
+    return columns
 
 
 def main():
@@ -111,4 +111,4 @@ def main():
     
 print("Welcome to Love Sandwiches Data Automation")
 #main()
-get_last_5_entries_sales()
+sales_columns = get_last_5_entries_sales()
